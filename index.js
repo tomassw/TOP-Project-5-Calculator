a663e286efa77481bc8c8d3dd4331ef56aff2ec8
tomassw <75063636+tomassw@users.noreply.github.com>
2022-11-13 10:16:29 +0200

    Started event handling
    - Solved so that the first character must be correct
    - Solved so that the second must be off those within the buttons

--- index.js
+++ index.js
@@ -16,6 +16,49 @@ const buttons = document.querySelectorAll('button');
 - folosesc .click() ca sa actioneze singur butonul
 */
 
-window.addEventListener('keydown', function (e) {
-    document.querySelector('#display').innerHTML = `You pressed ${e.key}`;
-  }, false);
+
+//Initializari
+let contor = 0;
+let expression;
+
+const display = document.querySelector('#display');
+
+
+//Keyboard action
+/*
+window.addEventListener('keydown', function(e) {
+  display.innerHTML = `You pressed ${e.key}`;
+  if (e.key === '1') e.key.click;
+}, false);  
+*/
+
+//Click action
+for (const button of buttons) {
+  button.addEventListener('click', function onClick() {
+
+    if (contor == 0 && ((button.value >= '1' && button.value <= ('9')) || button.value == '()'  || button.value == '-')) {
+      expression = `${button.value}`;
+
+      contor++;
+
+      display.innerHTML = expression;
+
+    } else if (contor > 0 && contor <= 10) {
+        if (contor <= 9) {
+          if (button.value >= '0' && button.value <='9') { 
+            expression += `${button.value}`;
+            contor++;
+            display.innerHTML = expression;
+          }
+        }
+        if (button.value == '=') {
+          contor = 0;
+          display.innerHTML = 'rezultat';
+        }
+        if (button.value == 'clear') {
+          display.innerHTML = '0';
+          contor = 0;
+        }
+    }
+  });
+}
